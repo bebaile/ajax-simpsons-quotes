dcf7467ad3e6e85fc391cc1ffc16f7d0fe7a5755
Basile Carle <basile.carle@edhec.com>
2022-04-25 10:17:27 +0200

    added button to get a new quote

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const API = "https://simpsons-quotes-api.herokuapp.com/quotes";
-axios
-  .get(API)
-  .then((resp) => resp.data)
-  .then((resp) => {
-    display(resp[0]);
-  });
+
+function getAnewQuote() {
+  axios
+    .get(API)
+    .then((resp) => resp.data)
+    .then((resp) => {
+      display(resp[0]);
+    });
+}
 
 function display(arr) {
   const divSelector = document.querySelector(".content");
   divSelector.innerHTML = `<h2>${arr.character}</h2><img src ="${arr.image}" alt= "${arr.character}"><div>${arr.quote}</div>`;
 }
+
+const buttonQuote = document.querySelector(".load-quote");
+buttonQuote.addEventListener("click", (e) => {
+  getAnewQuote();
+});
+
+getAnewQuote();
